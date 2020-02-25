09d5824d81083078da5bb74909053a22047a51ab
Nichole Mattera <697668+NicholeMattera@users.noreply.github.com>
2020-02-25 07:47:04 -0500

    More validation.

--- cogs/lists.py
+++ cogs/lists.py
@@ -86,6 +86,9 @@ class Lists(Cog):
         await message.edit(embed = None)
 
     async def link_list_item(self, ctx, channel: discord.TextChannel, number: int):
+        if number <= 0:
+            await ctx.send(f'Number must be greater than 0.')
+
         if channel.id not in config.list_channels:
             await ctx.send(f'{channel.mention} is not a list channel.')
             return
