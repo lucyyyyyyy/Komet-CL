6c84107e5d3dbb2704e9067a5c378ea813dae9e4
Lucy <54782568+lucyyyyyyy@users.noreply.github.com>
2020-05-09 23:39:57 +1000

    Enhance readability
    Thanks @NicholeMattera

--- cogs/basic.py
+++ cogs/basic.py
@@ -79,11 +79,11 @@ class Basic(Cog):
         await tmp.edit(content=message_text)
 
     @commands.command(aliases=['creport'])
-    async def modmail(self, ctx):
+    async def modmail(self, ctx, *, body: str = ""):
         """Sends a modmail"""
         modmail_channel = self.bot.get_channel(config.modmail_channel)
         embed =  discord.Embed(title=f"New modmail from {ctx.author.name}!",
-                               description=f"**Content:** {ctx.message.content}")
+                               description=f"**Content:** {body}")
         embed.set_thumbnail(url=ctx.author.avatar_url)
 
         await modmail_channel.send(embed=embed)
